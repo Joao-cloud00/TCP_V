78d9335ba99eefc7e96bd9f32f17ea2eb8669d5a
João Vitor Ribeiro <2015ribeirojv@gmail.com>
2025-02-20 23:23:42 -0300

    Anim de atk

--- TCP V/Assets/Scripts/PlayerAttack.cs
+++ TCP V/Assets/Scripts/PlayerAttack.cs
@@ -7,6 +7,7 @@ public class PlayerAttack : MonoBehaviour
     public int attackDamage = 10; // Dano do ataque
     public Transform attackPoint; // Ponto de ataque
     public Collider2D attackCollider; // Collider do ataque
+    public Animator anim;
 
     private void Start()
     {
@@ -18,6 +19,7 @@ public class PlayerAttack : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             StartAttack();
+            anim.SetTrigger("atk");
         }
     }
 
@@ -25,11 +27,13 @@ public class PlayerAttack : MonoBehaviour
     {
         attackCollider.enabled = true; // Ativa o Collider do ataque
         Invoke("EndAttack", 0.1f); // Desativa após um curto período para simular o golpe
+
     }
 
     void EndAttack()
     {
         attackCollider.enabled = false; // Desativa o Collider do ataque
+   
     }
 
     private void OnTriggerEnter2D(Collider2D other)
